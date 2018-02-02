8358fa2638b2e63923cb7a2c7e152798c47ac067
prashantmohan <prashant530@gmail.com>
2018-02-02 03:25:55 -0500

    Query to get the questions and answers

--- server.py
+++ server.py
@@ -222,7 +222,7 @@ def keywords(code):
 def metrics(code):
     conn = get_db()
     result_set = conn.cursor()
-    query = "SELECT count(an_key), count(ex_key) FROM tanl.questionanswer where tr_key='" + code + "'"
+    query = "SELECT count(an_key), count(ex_key), count(question) FROM tanl.questionanswer where tr_key='" + code + "'"
     result_set.execute(query);
     result = result_set.fetchall()
     return jsonify(result);
@@ -231,10 +231,22 @@ def metrics(code):
 def qanda(code):
     conn = get_db()
     result_set = conn.cursor()
-    query = "SELECT qa_key, tr_key, question, answer, questionasw, answerasw,ex_key,an_key FROM tanl.questionanswer where tr_key='" + code + "'"
+    query = "SELECT t.tr_key,t.year,t.quarter,qa.question,qa.qa_key FROM tanl.questionanswer qa " \
+            " left join transcripts t on t.tr_key=qa.tr_key  " \
+            " where qa.tr_key='" + code + "'"
+    result_set.execute(query);
+    result = result_set.fetchall();
+    return jsonify(result);
+
+@app.route("/keywordsbyqakey/<string:code>/", methods=['GET'])
+def keywordsbyqakey(code):
+    conn = get_db()
+    result_set = conn.cursor()
+    query = "SELECT k.qa_key,k.tr_key,k.keyword,k.sentiment FROM tanl.keywords k " \
+            " where k.qa_key='" + code + "' and k.type='Q'"
     result_set.execute(query);
     result = result_set.fetchall();
     return jsonify(result);
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True);
+    app.run(host="0.0.0.0", port=5000, debug=True)
