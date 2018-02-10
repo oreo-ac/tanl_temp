08698c771d9f2c82c8208bec49184b97755411e3
vasu <vasur86@gmail.com>
2018-02-10 18:54:40 -0500

    Updated UI

---
+++ helpers.py
@@ -0,0 +1,387 @@
+import requests
+from textblob import TextBlob
+import json
+
+es_url = "https://search-tanl-nswk7nthqjskczmapaqzga3c2q.us-east-1.es.amazonaws.com/"
+search_type_index = "search/types/"
+qanda_index = "qanda/details/"
+
+def get_search_types(text):
+    temp = TextBlob(text)
+    temp = temp.words
+    temp = "* OR *".join(temp)
+    url = es_url + search_type_index + "_search?size=10000&q=search_words:(*" + temp + "*)" 
+    response = requests.get(url=url)
+    if str(response.status_code) == "200":
+        results = json.loads(response.text)["hits"]
+    else:
+        results = []
+    return results
+
+def clean_search_text(search_text):
+    temp = TextBlob(search_text)
+    temp = temp.words
+
+    temp_year = list(filter(lambda d: len(str(d)) == 4, temp))
+    temp_year = list(filter(lambda d: str(d)[0] in ["1", "2", "3", "4", "5", "6", "7", "8", "9"], temp_year))
+    temp_year = list(filter(lambda d: str(d)[1] in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], temp_year))
+    temp_year = list(filter(lambda d: str(d)[2] in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], temp_year))
+    temp_year = list(filter(lambda d: str(d)[3] in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], temp_year))
+    year = []
+    if len(temp_year) > 0:
+        year = temp_year
+    temp_quarter = list(filter(lambda d: len(str(d)) == 2, temp))
+    temp_quarter = list(filter(lambda d: str(d)[0].lower() == "q", temp_quarter))
+    temp_quarter = list(filter(lambda d: str(d)[1] in ["1", "2", "3", "4"], temp_quarter))
+    quarter = []
+    if len(temp_quarter) > 0:
+        quarter = temp_quarter
+
+    yearquarter = []
+    if len(year) > 0:
+        for y in year:
+            if len(quarter) > 0:
+                for q in quarter:
+                    yearquarter.append({
+                        "bool" :
+                            { "must":[
+                                    { "term" : {"year.keyword" : str(y)}},
+                                    { "term" : {"quarter.keyword" : str(q).upper()}}
+                                ]
+                            }
+                    })
+            else:
+                yearquarter.append({
+                        "bool" :
+                            { "must":[
+                                    { "term" : {"year.keyword" : str(y)}}
+                                ]
+                            }
+                    })
+    else:
+        for q in quarter:
+            yearquarter.append({
+                "bool" :
+                    { "must":[
+                            { "term" : {"quarter.keyword" : str(q).upper()}}
+                        ]
+                    }
+            })
+    temp_ticker = []
+    for t in temp:
+        temp_ticker.append({
+            "term": {
+                "ticker.keyword": str(t).upper()
+            }
+        })
+    
+    return year, quarter, temp_ticker, yearquarter
+
+def get_data(search_type, inp_year, inp_quarter, temp_keywords, yearquarter):
+    
+    input_json = {
+        "_source": {
+            "includes": [ "year", "quarter", "question", "sentiment"]
+        },
+        "size": 1000,
+        "query" : {
+            "constant_score" : {
+                "filter" : {
+                "bool" : {
+                    "must" : [
+                    { "bool" : {
+                            "should" : temp_keywords
+                        }
+                    },
+                    { "bool" : {
+                        "should" : yearquarter
+                    }
+                    }    
+                    ]
+                }
+                }
+            }
+        },
+        "aggs":{
+            "tickerAgg":{
+                "terms":{
+                    "field":"ticker.keyword"
+                }
+            },
+            "analystAgg":{
+                "terms":{
+                    "field":"analyst_name.keyword"
+                },
+                "aggs":{
+                    "sentimentAgg":{
+                        "terms":{
+                            "field":"sentiment.keyword"
+                        }
+                    }
+                }
+            },
+            "yearAgg":{
+                "terms":{
+                    "field":"year.keyword"
+                },
+                "aggs":{
+                    "quarterAgg":{
+                        "terms":{
+                            "field":"quarter.keyword"
+                        },
+                        "aggs":{
+                            "tickerAgg":{
+                                "terms":{
+                                    "field":"ticker.keyword"
+                                },
+                                "aggs":{
+                                    "sentimentAgg":{
+                                        "terms":{
+                                            "field":"sentiment.keyword"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    url = es_url + qanda_index + "_search"
+    headers = {"Content-Type" : "application/json"}
+    response = requests.post(url=url, data=json.dumps(input_json), headers=headers)
+
+    total_questions = 0
+    total_positive_questions = 0
+    total_neutral_questions = 0
+    total_negative_questions = 0
+    total_transcripts = 0
+    total_analysts = 0
+
+    analyst_container = []
+    main_container = []
+    if str(response.status_code) == "200":
+        questions = []
+        questions_output = json.loads(response.text)["hits"]["hits"]   
+        for question in questions_output:
+            
+            questions.append({
+                "year": question["_source"]["year"],
+                "quarter": question["_source"]["quarter"],
+                "question": question["_source"]["question"],
+                "sentiment": question["_source"]["sentiment"]
+            })
+        
+        tickers = []
+        ticker_output = json.loads(response.text)["aggregations"]["tickerAgg"]["buckets"]    
+        for ticker in ticker_output:
+            tickers.append(ticker["key"])
+
+        #if search_type["_source"]["name"] == "sentiment":
+        if "sentiment" == "sentiment":
+            analyst_output = json.loads(response.text)["aggregations"]["analystAgg"]["buckets"]
+            
+            for analyst in analyst_output:
+                total_analysts = total_analysts + 1
+                positive_cnt = list(filter(lambda d: d["key"] == "positive", analyst["sentimentAgg"]["buckets"]))
+                if len(positive_cnt) > 0:
+                    positive_cnt = positive_cnt[0]["doc_count"]
+                else:
+                    positive_cnt = 0
+                
+                negative_cnt = list(filter(lambda d: d["key"] == "negative", analyst["sentimentAgg"]["buckets"]))
+                if len(negative_cnt) > 0:
+                    negative_cnt = negative_cnt[0]["doc_count"]
+                else:
+                    negative_cnt = 0
+
+                neutral_cnt = list(filter(lambda d: d["key"] == "neutral", analyst["sentimentAgg"]["buckets"]))
+                if len(neutral_cnt) > 0:
+                    neutral_cnt = neutral_cnt[0]["doc_count"]
+                else:
+                    neutral_cnt = 0
+                
+                total_cnt = positive_cnt + negative_cnt + neutral_cnt
+                total_questions = total_questions + total_cnt
+                total_positive_questions = total_positive_questions + positive_cnt
+                total_negative_questions = total_negative_questions + negative_cnt
+                total_neutral_questions = total_neutral_questions + neutral_cnt
+                if total_cnt > 0:
+                    positive_pct = (positive_cnt/total_cnt*100)
+                    negative_pct = (negative_cnt/total_cnt*100)
+                    neutral_pct = (neutral_cnt/total_cnt*100)
+                else:
+                    positive_pct = 0
+                    negative_pct = 0
+                    neutral_pct = 0
+                winner = ""
+                if positive_pct > negative_pct and positive_pct > neutral_pct:
+                    winner = "positive"
+                elif negative_pct > positive_pct and negative_pct > neutral_pct:
+                    winner = "negative"
+                else:
+                    winner = "neutral"
+
+                analyst_container.append({
+                    "name": analyst["key"],
+                    "positive_pct": round(positive_pct,0),
+                    "negative_pct": round(negative_pct,0),
+                    "neutral_pct": round(neutral_pct,0),
+                    "pattern": winner
+                })
+
+            main_output = json.loads(response.text)["aggregations"]["yearAgg"]["buckets"]
+            main_container = {}
+            temp_container = []
+            temp_container1 = []
+            temp_dict = {}
+            for year in main_output:
+                temp_dict = {}
+                for quarter in year["quarterAgg"]["buckets"]:
+                    for ticker in quarter["tickerAgg"]["buckets"]:
+                        total_transcripts = total_transcripts + 1
+                        positive_cnt = list(filter(lambda d: d["key"] == "positive", ticker["sentimentAgg"]["buckets"]))
+                        if len(positive_cnt) > 0:
+                            positive_cnt = positive_cnt[0]["doc_count"]
+                        else:
+                            positive_cnt = 0
+                        
+                        negative_cnt = list(filter(lambda d: d["key"] == "negative", ticker["sentimentAgg"]["buckets"]))
+                        if len(negative_cnt) > 0:
+                            negative_cnt = negative_cnt[0]["doc_count"]
+                        else:
+                            negative_cnt = 0
+
+                        neutral_cnt = list(filter(lambda d: d["key"] == "neutral", ticker["sentimentAgg"]["buckets"]))
+                        if len(neutral_cnt) > 0:
+                            neutral_cnt = neutral_cnt[0]["doc_count"]
+                        else:
+                            neutral_cnt = 0
+                        total_cnt = positive_cnt + negative_cnt + neutral_cnt
+                        if total_cnt > 0:
+                            positive_pct = (positive_cnt/total_cnt*100)
+                            negative_pct = (negative_cnt/total_cnt*100)
+                            neutral_pct = (neutral_cnt/total_cnt*100)
+                        else:
+                            positive_pct = 0
+                            negative_pct = 0
+                            neutral_pct = 0
+                        winner = ""
+
+                        if positive_pct > negative_pct and positive_pct > neutral_pct:
+                            winner = "positive"
+                        elif negative_pct > positive_pct and negative_pct > neutral_pct:
+                            winner = "negative"
+                        else:
+                            winner = "neutral"
+
+
+                        temp_dict = {
+                            "group": ticker["key"] + " " + str(year["key"]) + " " + str(quarter["key"]),
+                            "sort1": str(year["key"]) + " " + str(quarter["key"]) + ticker["key"],
+                            "group1": str(year["key"]) + " " + str(quarter["key"]),
+                            "ticker": ticker["key"],
+                            ticker["key"] + "-pv_pct": round(positive_pct, 0),
+                            ticker["key"] + "-nv_pct": round(negative_pct, 0),
+                            ticker["key"] + "-nt_pct": round(neutral_pct,0),
+                            "result": [
+                                {
+                                    "label": "positive",
+                                    "value": round(positive_pct, 0),
+                                    "color": "#00a65a"
+                                },
+                                {
+                                    "label": "negative",
+                                    "value": round(negative_pct, 0),
+                                    "color": "#f56954"
+                                },
+                                {
+                                    "label": "neutral",
+                                    "value": round(neutral_pct,0),
+                                    "color": "#3c8dbc"
+                                }
+                            ],
+                        }
+                        temp_dict["positive"] = round(positive_pct, 0)
+                        temp_dict["negative"] = round(negative_pct, 0)
+                        temp_dict["neutral"] = round(neutral_pct, 0)
+                        temp_container.append(temp_dict)
+
+
+            if len(search_type) > 0:
+                default_chart = search_type[0]["_source"]["default_chart"]
+            else:
+                default_chart = "pie"
+            result_dict = {}
+            y_keys = []
+            y_labels = []
+            colors = []
+            if len(tickers) > 1:
+                samples = [("positive","#00a65a"), ("negative", "#f56954"), ("neutral","#3c8dbc")]
+                for sample in samples:
+                    for ticker in tickers:
+                        #y_keys = y_keys + [ticker + "-positive", ticker + "-negative", ticker + "-neutral"]
+                        #y_labels =y_labels +  [ticker + "-positive", ticker + "-negative", ticker + "-neutral"]
+                        #colors = colors + ["#00a65a", "#f56954", "#3c8dbc"]
+                        y_keys.append(ticker + "-" + sample[0])
+                        y_labels.append(ticker + "-" + sample[0])
+                        colors.append(sample[1])
+                temp_loop = list(set([d["group1"] for d in temp_container]))
+                for cont in temp_loop:
+                    temp_list = list(filter(lambda d: d["group1"]== cont, temp_container))
+                    temp_dict = {
+                                "group1": cont,
+                                "sort1": cont
+                            }
+                    for li in temp_list:
+                        temp_dict[li["ticker"] + "-positive"] = li[li["ticker"] + "-pv_pct"]
+                        temp_dict[li["ticker"] + "-negative"] = li[li["ticker"] + "-nv_pct"]
+                        temp_dict[li["ticker"] + "-neutral"] = li[li["ticker"] + "-nt_pct"]
+                    temp_container1.append(temp_dict)
+                temp_container1 = sorted(temp_container1, key=lambda k: k['sort1'], reverse=True)
+                result_dict["data1"] = temp_container1
+            else:
+                y_keys = ["positive", "negative", "neutral"]
+                y_labels = ["positive", "negative", "neutral"]
+                colors = ["#00a65a", "#f56954", "#3c8dbc"]
+                temp_container = sorted(temp_container, key=lambda k: k['sort1'], reverse=True)
+                result_dict["data1"] = temp_container
+            result_dict["y_keys"] = y_keys
+            result_dict["y_labels"] = y_labels
+            result_dict["x_key"] = 'group1'
+            result_dict["chartType"] = default_chart
+            result_dict["colors"] = colors
+            result_dict["data"] = temp_container
+
+
+            main_container = result_dict
+        if total_transcripts == 0:
+            average_questions = 0
+            average_analysts = 0
+        else:
+            average_questions = round(total_questions/total_transcripts,0)
+            average_analysts = round(total_analysts/total_transcripts,0)
+        return {
+            "analyst_container": analyst_container,
+            "main_container": main_container,
+            "table_container": questions,
+            "search_results": {
+                "tickers": tickers,
+                "years": inp_year,
+                "quarters": inp_quarter,
+                "report_types": [] 
+            },
+            "metric_container": {
+                "total_questions": total_questions,
+                "total_positive_questions": total_positive_questions,
+                "total_negative_questions": total_negative_questions,
+                "total_neutral_questions": total_neutral_questions,
+                "average_questions": average_questions,
+                "total_analysts": total_analysts,
+                "average_analysts": average_analysts
+            }
+        }
+    else:
+        print("Error:" + str(response.text))
+        return []
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, g, jsonify
-import psycopg2
 import sys
 import json
 import base64
+import helpers
+
 from nltk.tokenize import word_tokenize, sent_tokenize
 import nltk
 from nltk.corpus import stopwords
@@ -15,27 +16,8 @@ try:
     nltk.download('punkt')
 except Exception as e:
     pass
-stop_words = set(stopwords.words('english'))
-stemmer = SnowballStemmer("english")
-
-stop_words.add(".")
-stop_words.add(",")
-stop_words.add("%")
-
 
 app = Flask(__name__, static_url_path='/assets', static_folder="assets")
-db_schema = "tanl"
-
-def get_db():
-    host="tanl.cgnbxyetzh4a.us-east-1.rds.amazonaws.com"
-    database="tanl"
-    db_usr="tanl"
-    db_pw=base64.b64decode("U2VtbWFrYXRhJDc=").decode('utf-8')
-    conn_string="host='{0}' dbname='{1}' user='{2}' password='{3}'".format(host, database, db_usr, db_pw)
-    db=getattr(g, '_database', None)
-    if db is None:
-        db=g._database = psycopg2.connect(conn_string)
-    return db
 
 @app.route("/")
 def hello():
@@ -43,198 +25,25 @@ def hello():
     return render_template("index.html", **locals())
 
 @app.route("/search/<string:code>/", methods=['GET'])
-def search(code):
-    code_list = word_tokenize(code.lower())
-    filtered_code = [w for w in code_list if not (w.lower() in stop_words or w.upper() in stop_words)]
-    filtered_code = [stemmer.stem(w) for w in filtered_code]
-    filtered_code = list(set(filtered_code))
-    conn = get_db()
-    row1_set = conn.cursor()
-    row2_set = conn.cursor()
-    row3_set = conn.cursor()
-    row4_set = conn.cursor()
-    custom_query = ""
-    
-    if len(filtered_code) > 0:
-        reference_codes = ["lower(code) like '%" + word + "%'" for word in filtered_code]
-        custom_query = "select distinct code, search_values from tanl.search_references where " + " or ".join(reference_codes)
-        row1_set.execute(custom_query)
-        
-        time_codes = ["lower(year || '' || quarter || '' || ticker || '' || company) like '%" + word + "%'" for word in filtered_code]
-        custom_query = "select distinct year,quarter, ticker, company, tr_key from tanl.transcripts where " + " or ".join(time_codes) + "  order by 1 desc limit 10"
-        row2_set.execute(custom_query)
-
-        time_codes = ["lower(year|| ' ' || quarter || ' of ' || ticker) like '%" + word + "%'" for word in filtered_code]
-        custom_query = "select distinct ticker from tanl.transcripts where " + " or ".join(time_codes) + "  order by 1 desc limit 5"
-        row3_set.execute(custom_query)
-
-        company_codes = ["lower(company) like '%" + word + "%'" for word in filtered_code]
-        custom_query = "select distinct company from tanl.transcripts where " + " or ".join(company_codes) + "  order by 1 desc limit 5"
-        row4_set.execute(custom_query)
-
-
-    if row1_set.rowcount == 0 or len(filtered_code) == 0:
-        custom_query = "select distinct code, search_values from tanl.search_references"
-        row1_set.execute(custom_query)
-    if row2_set.rowcount == 0 or len(filtered_code) == 0:
-        custom_query = "select distinct year,quarter, ticker, company, tr_key from tanl.transcripts order by 1 desc limit 10"
-        row2_set.execute(custom_query)
-    if row3_set.rowcount == 0 or len(filtered_code) == 0:
-        custom_query = "select distinct quarter from tanl.transcripts  order by 1 desc limit 5"
-        row3_set.execute(custom_query)
-    if row4_set.rowcount == 0 or len(filtered_code) == 0:
-        custom_query = "select distinct company, year||'-'||quarter, ticker from tanl.transcripts  order by 1 desc limit 5"
-        row4_set.execute(custom_query) 
-    result = {}
-
-    result1 = [row[0] + " " + row[1] for row in row1_set.fetchall()]
-    result2 = [list(row) for row in row2_set.fetchall()]
-    result3 = [row[0] for row in row3_set.fetchall()]
-    result4 = [row[0] for row in row4_set.fetchall()] 
-
-    temp = []
-    for row1 in result1:
-        for row2 in result2:
-            temp = row1 + " " + row2[2] + " (" + row2[3] + ") for " + str(row2[0])
-            if temp not in result:
-                result.update({temp: row2[4]})
-
-            temp = row1 + " " + row2[2] + " (" + row2[3] + ") for " + str(row2[0]) + " " + row2[1]
-            if temp not in result:
-                result.update({temp: row2[4]})
-
-            temp = row1 + " " + row2[2] + " (" + row2[3] + ") for " + str(row2[0]) + " " + row2[1]
-            if temp not in result:
-                result.update({temp: row2[4]})
-
-
-            temp = row1 + " " + row2[2] + " (" + row2[3] + ") for last 12 months"
-            if temp not in result:
-                result.update({temp: row2[4]})
-    return json.dumps(result)
-
-@app.route("/searchresults/<string:code>/", methods=['GET'])
 def search_results(code):
-    action_type = ""
-    year = ""
-    quarter = ""
-    ticker = ""
-    chart_type = "pie"
+    year, quarter, temp, yearquarter = helpers.clean_search_text(code)
+    search_response = helpers.get_search_types(code)
+    search_response = search_response["hits"]
     
-    try:
-        action_type = code.split(" ")[0]
-    except Exception as e:
-        return jsonify({}), 404
-
-    try:
-        ticker = code.split(" ")[2]
-    except Exception as e:
-        return jsonify({}), 404
-
-    filter_text = code.split(" for ")[-1]
-    clause = ""
-
-    group_query = "select year,quarter from tanl.transcripts where lower(ticker)='{0}'".format(ticker.lower())
-    if filter_text == "last 12 months":
-        clause = "order by year || quarter desc LIMIT 4"
-    else:
-        if len(filter_text.split(" ")) == 2:
-            year = filter_text.split(" ")[0]
-            quarter = filter_text.split(" ")[1]
-            clause = " and lower(quarter)='{0}' and year={1} order by year || quarter desc".format(quarter.lower(), str(year))
-        else:
-            print("Debugger")
-            print(filter_text)
-            year = filter_text
-            clause = " and year={0} order by year || quarter desc".format(str(year))
-
-    conn = get_db()
-    row1_set = conn.cursor()
-    row2_set = conn.cursor()
-    custom_query = "select query from tanl.search_references where code='{0}'".format(action_type)
-    row1_set.execute(custom_query)
-    data_query = ""
-    for row in row1_set.fetchall():
-        data_query = row[0]
-
-    temp_group_result = []
-    group_query =  "select * from (" + group_query + clause + ") as temp order by year ||quarter"
-    row1_set.execute(group_query)
-    for row in row1_set.fetchall():
-        where_clause = " and lower(quarter)='{0}' and year={1} and lower(t.ticker)='{2}'".format(row[1].lower(), str(row[0]), ticker.lower())
-        final_query = data_query + where_clause
-        row2_set.execute(final_query)
-        print(final_query)
-        temp_result = []
-        pos_cnt = 0
-        neg_cnt = 0
-        neu_cnt = 0
-        for row2 in row2_set.fetchall():
-            temp_result.append(json.loads(row2[0]))
-            if action_type == "sentiment":
-                pos_cnt = row2[1]
-                neg_cnt = row2[2]
-                neu_cnt = row2[3]
-        temp_dict = {
-            "group": str(row[0]) + ' ' + row[1],
-            "result": temp_result
+    if 0 == 1:
+        response = {
+            "analyst_container": [],
+            "main_container": []
         }
-        if action_type == "sentiment":
-            temp_dict["positive"] = pos_cnt
-            temp_dict["negative"] = neg_cnt
-            temp_dict["neutral"] = neu_cnt
-        temp_group_result.append(temp_dict)
+    else:
+        response = helpers.get_data(search_response, year, quarter, temp, yearquarter)
+        
+    return jsonify(response)
 
-    result_dict = {}
-    if action_type == "sentiment":
-        x_keys = ["positive", "negative", "neutral"]
-        x_labels = ["positive", "negative", "neutral"]
-        colors = ["#00a65a", "#f56954", "#3c8dbc"]
-        result_dict["y_keys"] = x_keys
-        result_dict["y_labels"] = x_labels
-        result_dict["x_key"] = 'group'
-        result_dict["chartType"] = chart_type
-        result_dict["colors"] = colors
-    result_dict["data"] = temp_group_result
-    return jsonify(result_dict)
-
-@app.route("/getqanda/<string:code>/", methods=['GET'])
-def getqanda(code):
-    qanda = {}
-    conn = get_db()
-    result_set = conn.cursor()
-    query =  "SELECT qa_key, tr_key, question, answer, questionasw, answerasw FROM tanl.questionanswer where tr_key='" + code + "' ORDER BY RANDOM() LIMIT 1"
-    result_set.execute(query);
-    result = result_set.fetchall();
-    qanda.update({result[0][2]: result[0][3]});
-    return jsonify(qanda);
-
-@app.route("/keywords/<string:code>/", methods=['GET'])
-def keywords(code):
-    conn = get_db()
-    result_set = conn.cursor()
-    query =  "SELECT qa_key, tr_key, type, keyword, count FROM tanl.keywords where tr_key='" + code + "' ORDER BY RANDOM() LIMIT 5"
-    result_set.execute(query);
-    result = [row[3] for row in result_set.fetchall()]
-    return jsonify(result);
-
-@app.route("/metrics/<string:code>/", methods=['GET'])
-def metrics(code):
-    conn = get_db()
-    result_set = conn.cursor()
-    query = "SELECT count(an_key), count(ex_key) FROM tanl.questionanswer where tr_key='" + code + "'"
-    result_set.execute(query);
-    result = result_set.fetchall()
-    return jsonify(result);
-
-@app.route("/qanda/<string:code>/", methods=['GET'])
-def qanda(code):
-    conn = get_db()
-    result_set = conn.cursor()
-    query = "SELECT qa_key, tr_key, question, answer, questionasw, answerasw,ex_key,an_key FROM tanl.questionanswer where tr_key='" + code + "'"
-    result_set.execute(query);
-    result = result_set.fetchall();
-    return jsonify(result);
+@app.route("/test/api/<string:searchtext>/", methods=["GET"])
+def test_api(searchtext):
+    response = helpers.get_search_types(searchtext)
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True);
